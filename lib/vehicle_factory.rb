d3f712740d8f7e06a7e4aa829d13a5c5b32b2c5a
Kyle Delaney <khdelaney99@gmail.com>
2024-07-14 13:49:57 -0400

    add initialize method and fix transform_vehicle_data method for vehicle factory

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -2,10 +2,21 @@ require './lib/dmv_data_service'
 require './lib/vehicle'
 
 class VehicleFactory
-    
-    
+
+    attr_accessor :vehicle_details,
+                  :vehicles
+
+    def initialize
+        @vehicle_details = {vin: nil, year: nil, make: nil, model: nil, engine: :ev, plate_type: nil, registration_date: nil}
+        @vehicles = []
+    end
+
     def transform_vehicle_data(vehicle) 
-        
+        @vehicle_details [:vin] = vehicle[:vin_1_10]
+        @vehicle_details [:year] = vehicle[:model_year]
+        @vehicle_details [:make] = vehicle[:make]
+        @vehicle_details [:model] = vehicle[:model]
+        @vehicles.push(Vehicle.new(@vehicle_details))
     end
 
     def create_vehicles(vehicle_list)
@@ -13,3 +24,4 @@ class VehicleFactory
     end
 
 end
+
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -10,38 +10,48 @@ RSpec.describe VehicleFactory do
     before(:each) do
         @factory = VehicleFactory.new
         @wa_ev_registrations = DmvDataService.new.wa_ev_registrations    
-        @factory.transform_vehicle_data(@wa_ev_registrations[0])
+        @test_car = @wa_ev_registrations[0]
     end
 
-    describe '#transform_vehicle_data' do
+        describe '#initialize' do
+            it 'will create a vehicle_details object with all nil values(excludes engine:)' do
+                expect(@factory.vehicle_details).to eq({:vin => nil, :year => nil, :make => nil, :model => nil, :engine => :ev, :plate_type => nil, :registration_date => nil})
+            end
 
-        it 'will create a new Vehicle object' do
-            expect(@factory.transform_vehicle_data(@wa_ev_registrations[0])).to be_an_instance_of Vehicle
+            it 'will create a vehicles array to store vehicle instances' do
+                expect(@factory.vehicles).to be_an_instance_of Array
+            end
         end
 
-        it 'will convert relevant API object key value pairs to object for Vehicle class' do
-            @factory.transform_vehicle_data(@wa_ev_registrations[0])
 
-            expect(@wa_ev_registrations[0].vehicle_details).to eq {vin: '3C3CFFGE5F', year: 2015, make: 'FIAT', model: '500', engine: :ev, plate_type: nil, registration_date: nil}
+        describe '#transform_vehicle_data' do
+            #describe'pry';binding.pry
+
+            it 'will create a new Vehicle object' do
+                @factory.transform_vehicle_data(@test_car)
+
+                expect(@factory.vehicles[0]).to be_an_instance_of Vehicle
+            end
+
+            it 'will apply relevant key value pairs to Vehicle object as argument for vehicle details' do
+                @factory.transform_vehicle_data(@test_car)
+
+                expect(@factory.vehicles[0].vin).to eq '3C3CFFGE5F'
+                expect(@factory.vehicles[0].year).to eq '2015'
+                expect(@factory.vehicles[0].make).to eq 'FIAT'
+                expect(@factory.vehicles[0].model).to eq '500'
+                expect(@factory.vehicles[0].engine).to eq :ev
+                expect(@factory.vehicles[0].plate_type).to eq nil
+                expect(@factory.vehicles[0].registration_date).to eq nil
+            end
         end
 
-        it 'will apply key value pairs to Vehicle object as argument for instantiation' do
-            expect(@wa_ev_registrations[0].vin).to eq '3C3CFFGE5F'
-            expect(@wa_ev_registrations[0].year).to eq 2015
-            expect(@wa_ev_registrations[0].make).to eq 'FIAT'
-            expect(@wa_ev_registrations[0].model).to eq '500'
-            expect(@wa_ev_registrations[0].engine).to eq :ev
-            expect(@wa_ev_registrations[0].plate_type).to eq nil
-            expect(@wa_ev_registrations[0].registration_date).to eq nil
+        describe '#create_vehicles' do
+
+            it 'will push vehicles to the vehicles array using the #transform_vehicle_data helper method' do 
+
+            end
         end
-    end
-
-    describe '#create_vehicles' do
-
-        it 'will push vehicles to the vehicles array using the #transform_vehicle_data helper method' do 
-
-        end
-    end
     
 
 end
