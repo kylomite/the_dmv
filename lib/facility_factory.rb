f09d96dc841848de5d96b5054ce0abb6d24fcfe0
Kyle Delaney <khdelaney99@gmail.com>
2024-07-15 08:47:34 -0400

    add and pass tests for NY #facility_factory methods & add tests for MO #facility_factory methods

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,18 +4,42 @@ require './lib/facility'
 
 class Facility_Factory 
     attr_accessor :facility_details, 
-                  :facilites
+                  :facilities
 
     def initialize 
-        @facility_details = [
-            {:name => nil, :address => nil, :phone => nil, :services => [], :registered_vehicles => [], :collected_fees => 0},
-            {:name => nil, :address => nil, :phone => nil, :services => [], :registered_vehicles => [], :collected_fees => 0},
-            {:name => nil, :address => nil, :phone => nil, :services => [], :registered_vehicles => [], :collected_fees => 0}
-        ]
-        @facilites = {
+        @facility_details = {:name => nil, :address => nil, :phone => nil, :services => [], :registered_vehicles => [], :collected_fees => 0}
+        @facilities = {
             :co => [],
             :ny => [],
             :mo => []
         }
+    end
+
+    def transform_co_facility_data(facility)
+        @facility_details [:name] = facility[:dmv_office]
+        @facility_details [:address] = facility[:address_li]
+        @facility_details [:phone] = facility[:phone]
+        @facilities[:co].push(Facility.new(@facility_details))    
+    end
+
+    def create_co_facilities(facilities_list)
+        facilities_list.each {|facility|
+        transform_co_facility_data(facility)
+        }
+        @facilities[0]
+    end
+
+    def transform_ny_facility_data(facility)
+        @facility_details [:name] = facility[:office_name]
+        @facility_details [:address] = facility[:street_address_line_1]
+        @facility_details [:phone] = facility[:public_phone_number]
+        @facilities[:ny].push(Facility.new(@facility_details))    
+    end
+
+    def create_ny_facilities(facilities_list)
+        facilities_list.each {|facility|
+        transform_ny_facility_data(facility)
+        }
+        @facilities[1]
     end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -18,12 +18,12 @@ RSpec.describe Facility_Factory do
     end
     
     describe '#initialize' do
-        it 'will create an array of faciltiy_details object templates with default values for each state' do
-            expect(@dmv.facility_details[0]).to eq({:name => nil, :address => nil, :phone => nil, :services => [], :registered_vehicles => [], :collected_fees => 0})
+        it 'will create an array of faciltiy_details object templates with default values' do
+            expect(@dmv.facility_details).to eq({:name => nil, :address => nil, :phone => nil, :services => [], :registered_vehicles => [], :collected_fees => 0})
         end
 
         it 'will create an object to store facility instances for each state' do
-            expect(@dmv.facilites).to eq({:co => [], :ny => [], :mo => []})
+            expect(@dmv.facilities).to eq({:co => [], :ny => [], :mo => []})
         end
     end
 #COLORADO
@@ -31,18 +31,18 @@ RSpec.describe Facility_Factory do
         it 'will create a new Facility object' do
             @dmv.transform_co_facility_data(@co_test_facility)
 
-                expect(@dmv.facilites[:co][0]).to be_an_instance_of Facility
+                expect(@dmv.facilities[:co][0]).to be_an_instance_of Facility
         end
 
         it 'will apply relevant key value pairs to Facility object as argument for facility details' do
             @dmv.transform_co_facility_data(@co_test_facility)
 
-                expect(@dmv.facilites[:co][0].name).to eq 'DMV Tremont Branch'
-                expect(@dmv.facilites[:co][0].address).to eq '2855 Tremont Place'
-                expect(@dmv.facilites[:co][0].phone).to eq '(720) 865-4600'
-                expect(@dmv.facilites[:co][0].services).to eq []
-                expect(@dmv.facilites[:co][0].registered_vehicles).to eq []
-                expect(@dmv.facilites[:co][0].collected_fees).to eq 0
+                expect(@dmv.facilities[:co][0].name).to eq 'DMV Tremont Branch'
+                expect(@dmv.facilities[:co][0].address).to eq '2855 Tremont Place'
+                expect(@dmv.facilities[:co][0].phone).to eq '(720) 865-4600'
+                expect(@dmv.facilities[:co][0].services).to eq []
+                expect(@dmv.facilities[:co][0].registered_vehicles).to eq []
+                expect(@dmv.facilities[:co][0].collected_fees).to eq 0
         end
 
         it 'will push the facility instance into the facilitys state array' do
@@ -50,28 +50,29 @@ RSpec.describe Facility_Factory do
         end
     end
 
-    describe 'create_co_facilities' do
-        it 'will push facilites to the appropriate state array using the #transform_facility_data helper method' do
-            expect{@dmv.transform_co_facility_data(@co_test_facility)}.to change{@dmv.facilities[:co].length}.from(0).to(5)
+    describe '#create_co_facilities' do
+        it 'will push facilities to the appropriate state array using the #transform_facility_data helper method' do
+        #require'pry';binding.pry
+            expect{@dmv.create_co_facilities(@co_facilities)}.to change{@dmv.facilities[:co].length}.from(0).to(5)
         end
     end
-#NEW YORK
+# #NEW YORK
        describe '#transform_ny_facility_data' do
         it 'will create a new Facility object' do
             @dmv.transform_ny_facility_data(@ny_test_facility)
 
-                expect(@dmv.facilites[:ny][0]).to be_an_instance_of Facility
+                expect(@dmv.facilities[:ny][0]).to be_an_instance_of Facility
         end
 
         it 'will apply relevant key value pairs to Facility object as argument for facility details' do
             @dmv.transform_ny_facility_data(@ny_test_facility)
 
-                expect(@dmv.facilites[:co][0].name).to eq 'HUNTINGTON'
-                expect(@dmv.facilites[:co][0].address).to eq '1815 E JERICHO TURNPIKE'
-                expect(@dmv.facilites[:co][0].phone).to eq '7184774820'
-                expect(@dmv.facilites[:co][0].services).to eq []
-                expect(@dmv.facilites[:co][0].registered_vehicles).to eq []
-                expect(@dmv.facilites[:co][0].collected_fees).to eq 0
+                expect(@dmv.facilities[:ny][0].name).to eq 'HUNTINGTON'
+                expect(@dmv.facilities[:ny][0].address).to eq '1815 E JERICHO TURNPIKE'
+                expect(@dmv.facilities[:ny][0].phone).to eq '7184774820'
+                expect(@dmv.facilities[:ny][0].services).to eq []
+                expect(@dmv.facilities[:ny][0].registered_vehicles).to eq []
+                expect(@dmv.facilities[:ny][0].collected_fees).to eq 0
         end
 
         it 'will push the facility instance into the facilitys state array' do
@@ -79,28 +80,28 @@ RSpec.describe Facility_Factory do
         end
     end
 
-    describe 'create_co_facilities' do
-        it 'will push facilites to the appropriate state array using the #transform_facility_data helper method' do
-            expect{@dmv.transform_ny_facility_data(@ny_test_facility)}.to change{@dmv.facilities[:ny].length}.from(0).to(170)
+    describe 'create_ny_facilities' do
+        it 'will push facilities to the appropriate state array using the #transform_facility_data helper method' do
+            expect{@dmv.create_ny_facilities(@ny_facilities)}.to change{@dmv.facilities[:ny].length}.from(0).to(170)
         end
     end
-#MISSOURI
+# #MISSOURI
     describe '#transform_mo_facility_data' do
         it 'will create a new Facility object' do
             @dmv.transform_mo_facility_data(@mo_test_facility)
 
-                expect(@dmv.facilites[:mo][0]).to be_an_instance_of Facility
+                expect(@dmv.facilities[:mo][0]).to be_an_instance_of Facility
         end
 
         it 'will apply relevant key value pairs to Facility object as argument for facility details' do
             @dmv.transform_mo_facility_data(@mo_test_facility)
 
-                expect(@dmv.facilites[:co][0].name).to eq 'FERGUSON-OFFICE CLOSED UNTIL FURTHER NOTICE'
-                expect(@dmv.facilites[:co][0].address).to eq '10425 WEST FLORISSANT'
-                expect(@dmv.facilites[:co][0].phone).to eq '(314) 733-5316'
-                expect(@dmv.facilites[:co][0].services).to eq []
-                expect(@dmv.facilites[:co][0].registered_vehicles).to eq []
-                expect(@dmv.facilites[:co][0].collected_fees).to eq 0
+                expect(@dmv.facilities[:mo][0].name).to eq 'FERGUSON-OFFICE CLOSED UNTIL FURTHER NOTICE'
+                expect(@dmv.facilities[:mo][0].address).to eq '10425 WEST FLORISSANT'
+                expect(@dmv.facilities[:mo][0].phone).to eq '(314) 733-5316'
+                expect(@dmv.facilities[:mo][0].services).to eq []
+                expect(@dmv.facilities[:mo][0].registered_vehicles).to eq []
+                expect(@dmv.facilities[:mo][0].collected_fees).to eq 0
         end
 
         it 'will push the facility instance into the facilitys state array' do
@@ -108,9 +109,10 @@ RSpec.describe Facility_Factory do
         end
     end
 
-    describe 'create_co_facilities' do
-        it 'will push facilites to the appropriate state array using the #transform_facility_data helper method' do
-            expect{@dmv.transform_mo_facility_data(@mo_test_facility)}.to change{@dmv.facilities[:mo].length}.from(0).to(178)
+    describe 'create_mo_facilities' do
+        it 'will push facilities to the appropriate state array using the #transform_facility_data helper method' do
+        require 'pry';binding.pry
+            expect{@dmv.create_mo_facilities(@mo_facilities)}.to change{@dmv.facilities[:mo].length}.from(0).to(178)
         end
     end
 end
